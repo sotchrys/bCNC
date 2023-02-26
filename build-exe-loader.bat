0f6287179787195c99e2cbfa85938b1004f543c2
sotchrys <sotchryss@gmail.com>
2023-02-26 10:09:43 +0200

    Update build-exe-loader.bat

--- build-exe-loader.bat
+++ build-exe-loader.bat
@@ -1,4 +1,4 @@
-pip2 install pyinstaller
-pip2 install --upgrade setuptools
+pip install pyinstaller
+pip install --upgrade setuptools
 pyinstaller --onefile --distpath . --hidden-import tkinter --paths bCNC --icon bCNC/bCNC.ico --name bCNC bCNC.py
 pause
